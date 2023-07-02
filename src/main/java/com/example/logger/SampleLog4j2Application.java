6e61474e0c8145ff12f7cf10c48cd2fa0b50a56f
Soo <fgump78@gmail.com>
2023-07-02 20:26:38 +0900

    initialize project

---
+++ src/main/java/com/example/logger/SampleLog4j2Application.java
@@ -0,0 +1,13 @@
+package com.example.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleLog4j2Application.class, args);
+	}
+
+}
